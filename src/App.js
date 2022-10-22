55e24cfe3239638619dba94a23c91716487208af
PavitarSharma <pavitarsharma144@gmail.com>
2022-10-22 13:03:01 +0530

    react admin dashboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Routes, Route } from "react-router-dom"
+import { ColorModeContext, useMode } from "./theme"
+import { CssBaseline, ThemeProvider } from "@mui/material"
+import { Navbar, Sidebar } from "./components"
+import { Dashboard, Contacts, Invoices, Form, Faq, Bar, Line, Pie, Geography, Calendar, Team } from './pages'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const App = () => {
+    const [theme, colorMode] = useMode()
+
+    return (
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <div className="app">
+                    <Sidebar />
+                    <main className="content">
+                        <Navbar />
+                        <Routes>
+                            <Route exact path="/" element={<Dashboard />} />
+                            <Route path="/team" element={<Team />} />
+                            <Route path="/contacts" element={<Contacts />} />
+                            <Route path="/invoices" element={<Invoices />} />
+                            <Route path="/form" element={<Form />} />
+                            <Route path="/bar" element={<Bar />} />
+                            <Route path="/pie" element={<Pie />} />
+                            <Route path="/line" element={<Line />} />
+                            <Route path="/geography" element={<Geography />} />
+                            <Route path="/faq" element={<Faq />} />
+                            <Route path="/team" element={<Team />} />
+                            <Route path="/calendar" element={<Calendar />} />
+                        </Routes>
+                    </main>
+                </div>
+            </ThemeProvider>
+        </ColorModeContext.Provider>
+    )
 }
 
-export default App;
+export default App
